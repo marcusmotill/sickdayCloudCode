40c70bc164bbc6529d3fcedc256e26dcceb97540
Marcus Motill <mmotill@interapt.com>
2015-03-01 23:28:25 -0500

    Sending email and some user data

--- cloud/main.js
+++ cloud/main.js
@@ -1,13 +1,50 @@
+Parse.Cloud.beforeSave("Sickday_Requests", function(request, response) {
 
-Parse.Cloud.afterSave("Sickday_Requests", function(request) {
-    query = new Parse.Query("Sickday_Requests");
-    query.get(request.object.id, {
-        success: function(post) {
-            post.set("request_recieved", true);
-            post.save();
+    request.object.set("request_recieved", true);
+    response.success();
+    
+    
+    var insuranceObject;
+    var insuranceName;
+    var query = new Parse.Query("Insurance");
+    query.equalTo("username", request.user.get("email"));
+                  
+    query.first({
+        success: function(object) {
+            insuranceObject = object;
+            insuranceName = object.get("insuranceName");
+            console.log("there was no error");
         },
         error: function(error) {
-            console.error("Got an error " + error.code + " : " + error.message);
+            console.log("There was an error");
         }
     });
+    
+    
+    var Mandrill = require('mandrill');
+    Mandrill.initialize('NaNCUaLV1FuquyEE6jhC2Q');
+    
+    Mandrill.sendEmail({
+      message: {
+        text: "Name: " + request.user.get("firstname") + " " + request.user.get("lastname") + "\n" + "Insurance name: " + insuranceName,
+        subject: "New Sickday request",
+        from_email: request.user.get("email"),
+        from_name: request.user.get("firstname") + " " + request.user.get("lastname"),
+        to: [
+          {
+            email: "marcusmotill@gmail.com",
+            name: "Your Name"
+          }
+        ]
+      },
+      async: true
+    },{
+      success: function(httpResponse) {
+        console.log(httpResponse);
+      },
+      error: function(httpResponse) {
+        console.error(httpResponse);
+      }
+    });
+          
 });
